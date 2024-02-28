fc4c9a9b99f37c65d9085bb119d02ec0724f6c5f
Neelam Jain <neelamrghjain@gmail.com>
2024-02-28 12:44:39 +0530

    backend

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,9 +1,39 @@
+const errorHandler = require("../utils/error")
+const bcryptjs=require('bcryptjs')
+const { User } = require("../models/user.model")
  const test=(req,res)=>{
     res.json({
         msg:'api route is working'
     })
 }
+const updateUser=async (req,res,next)=>{
+if(req.user.id !==req.params.id) return next(errorHandler(401,'You can only update your own account!'))
+try {
+    if(req.body.password){
+        req.body.password=bcryptjs.hashSync(req.body.password,10)
+    }
+    const updatedUser=await User.findByIdAndUpdate(req.params.id,{
+        $set:{
+            username:req.body.username,
+            email:req.body.email,
+            password:req.body.password,
+            avatar:req.body.avatar,
+        }
+    },{new:true})
+
+    const {password,...rest}=updatedUser._doc
+    res.status(200).json({
+        rest
+    })
+} catch (error) {
+    next(error)
+}
+}
+
+
+
+
 
 module.exports={
-    test
+    test,updateUser
 }
--- index.js
+++ index.js
@@ -5,14 +5,16 @@ const dotenv=require('dotenv')
 dotenv.config();
 const authRouter=require("./routes/auth.route.js")
 const userRouter=require('./routes/user.route.js')
+const cookieParser=require('cookie-parser')
 mongoose.connect(process.env.MONGO).then(()=>{
     console.log('Connected to db')
 })
-.catch((err)=>{
-     console.log(err)
+.catch((error)=>{
+     console.log(error)
 })
 
 app.use(express.json())
+app.use(cookieParser());
 app.listen(3001,()=>{
     console.log('Server is running on port 3001 !!!')
 })
@@ -20,3 +22,12 @@ app.listen(3001,()=>{
 app.use('/api/user',userRouter)
 app.use('/api/auth',authRouter)
 
+app.use((error,req,res,next)=>{
+    const statusCode=error.statusCode || 500;
+    const message=error.message ||'Internal Server Error';
+    return res.status(statusCode).json({
+        success:false,
+        statusCode,
+        message
+    })
+})
