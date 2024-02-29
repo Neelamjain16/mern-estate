7b8e67264d6cdae77d0571758eba7b42bab27eed
Neelam Jain <neelamrghjain@gmail.com>
2024-02-29 11:14:26 +0530

    add delete user functionality

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -35,7 +35,18 @@ const { User } = require("../models/user.model")
     }
   };
 
+const deleteUser=async(req,res,next)=>{
+if(req.user.id !=req.params.id) return next(errorHandler(401,'You can only delete your own account!'))
+
+try {
+  await User.findByIdAndDelete(req.params.id)
+  res.clearCookie('access_token');
+  res.status(200).json('User has been deleted')
+} catch (error) {
+  next(error)
+}
+}
 
 module.exports={
-    test,updateUser
+    test,updateUser,deleteUser
 }
