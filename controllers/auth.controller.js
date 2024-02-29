f928d37e7577817973fc9dc2b4dca7aec8c20b38
Neelam Jain <neelamrghjain@gmail.com>
2024-02-29 11:31:13 +0530

    add signout functionality

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -55,6 +55,14 @@ try {
 }
 }
 
+const signOut=async(req,res,next)=>{
+try {
+  res.clearCookie('access_token');
+  res.status(200).json('User has been logged out');
+} catch (error) {
+  next(error)
+}
+}
 module.exports = {
-  signup,signin,google
+  signup,signin,google,signOut
 }
