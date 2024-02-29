11e5125e06de439d0d5353543e08dab65b25cb44
Neelam Jain <neelamrghjain@gmail.com>
2024-02-29 11:31:31 +0530

    add signout functionality

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -4,7 +4,7 @@ import { useRef } from 'react'
 import { useState, useEffect } from 'react';
 import { getDownloadURL, getStorage, ref, uploadBytesResumable } from 'firebase/storage'
 import { app } from '../firebase.js'
-import {updateUserStart,updateUserSuccess,updateUserFailure, deleteUserFailure, deleteUserStart, deleteUserSuccess} from '../redux/user/userSlice.js'
+import {updateUserStart,updateUserSuccess,updateUserFailure, deleteUserFailure, deleteUserStart, deleteUserSuccess, signOutUserStart, signOutUserSuccess, signOutUserFailure} from '../redux/user/userSlice.js'
 function Profile() {
   const fileRef = useRef(null);
   const { currentUser,loading,error } = useSelector((state) => state.user)
@@ -92,6 +92,21 @@ function Profile() {
       dispatch(deleteUserFailure(error.message))
     }
    }
+
+   const handleSignout=async()=>{
+    try {
+      dispatch(signOutUserStart());
+      const res=await fetch('/api/auth/signout');
+      const data=await res.json();
+      if(data.success===false){
+        dispatch(signOutUserFailure(data.message));
+        return;
+      }
+      dispatch(signOutUserSuccess(data));
+    } catch (error) {
+      dispatch(signOutUserFailure(error.message))
+    }
+   }
   return (
     <div className='p-3 max-w-lg mx-auto'>
       <h1 className='text-3xl font-semibold text-center my-7'>Profile</h1>
@@ -118,7 +133,7 @@ function Profile() {
       </form>
       <div className='flex justify-between mt-5'>
         <span className='text-red-700 cursor-pointer' onClick={handleDeleteUser}>Delete account</span>
-        <span className='text-red-700 cursor-pointer'>Sign out</span>
+        <span className='text-red-700 cursor-pointer' onClick={handleSignout}>Sign out</span>
       </div>
       <p className='text-red-700 mt-5'>{error? error:" "}</p>
       <p className='text-green-700 mt-5'>{updateSuccess? "User is updated successfully!":" "}</p>
